38ce595bc46f02ed8367cdb78fd7bef8c1d85624
dusixian <1912647548@qq.com>
2023-12-07 19:40:49 +0800

    pytest for ac func

--- test/examples/test_ac_function.py
+++ test/examples/test_ac_function.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 from ConfigSpace import ConfigurationSpace
 from openbox.acquisition_function.acquisition import (
+    AbstractAcquisitionFunction,
     EI,
     EIC,
     EIPS,
@@ -39,10 +40,29 @@ class MockModelDual:
             [np.mean(X, axis=1).reshape((1, -1))] * self.num_targets
         ).reshape((-1, 2))
     
+class MockModelDual_zerovar:
+    def __init__(self, num_targets=1):
+        self.num_targets = num_targets
+
+    def predict_marginalized_over_instances(self, X):
+        return np.array([np.mean(X, axis=1).reshape((1, -1))] * self.num_targets).reshape((-1, 2)), np.array(
+            [np.full(X.shape[0], 0.0).reshape((1, -1))] * self.num_targets
+        ).reshape((-1, 2))
+    
 class MockConstraintModel:
     def predict_marginalized_over_instances(self, X):
         m = -np.abs(np.mean(X, axis=1)).reshape((-1, 1))
         v = np.full(m.shape, 0.1)
+        return m, v
+    
+class MockModelWithNaN:
+    def predict_marginalized_over_instances(self, X):
+        return np.full(X.shape[0], np.nan).reshape((-1, 1)), np.full(X.shape[0], 1.0).reshape((-1, 1))
+    
+class MockModelWrongVShape:
+    def predict_marginalized_over_instances(self, X):
+        m = np.mean(X, axis=1).reshape((-1, 2))
+        v = np.var(X, axis=1).reshape((-1, 1)) 
         return m, v
 
 
@@ -51,12 +71,30 @@ def model():
     return MockModel()
 
 @pytest.fixture
+def model_eips():
+    return MockModelDual()
+
+@pytest.fixture
+def model_eips_zerovar():
+    return MockModelDual_zerovar()
+
+@pytest.fixture
 def constraint_model():
     return MockConstraintModel()
 
 @pytest.fixture
+def model_with_nan():
+    return MockModelWithNaN()
+
+@pytest.fixture
+def model_wrong_v_shape():
+    return MockModelWrongVShape()
+
+@pytest.fixture
 def acquisition_function(model):
     return EI(model)
+
+
 
 # --------------------------------------------------------------
 # Test AbstractAcquisitionFunction
@@ -74,6 +112,23 @@ def test_update_with_kwargs(acquisition_function):
     acquisition_function.update(model="abc", eta=0.0, other="hi there:)")
     assert acquisition_function.model == "abc"
 
+def test_call_with_nan(model_with_nan):
+    ei = EI(model=model_with_nan)
+    ei.update(eta=1.0)
+
+    configurations = [ConfigurationMock([0.5])]
+    acq = ei(configurations)
+
+    assert np.all(acq == -np.finfo(np.float64).max)
+
+def test_convert_false_with_1x1(model, acquisition_function):
+    ei = acquisition_function
+    ei.update(model=model, eta=1.0)
+    configurations = np.array([0.5])
+    acq = ei(configurations, convert=False)
+    assert acq.shape == (1, 1)
+    assert np.isclose(acq[0][0], 0.5998206141871228)
+
 
 # --------------------------------------------------------------
 # Test EI
@@ -83,6 +138,14 @@ def test_ei_init(acquisition_function):
     ei = acquisition_function
     assert ei.long_name == 'Expected Improvement'
     assert abs(ei.par) < 1e-5
+    assert str(ei) == 'EI (Expected Improvement)'
+
+def test_ei_eta_fail(model, acquisition_function):
+    ei = acquisition_function
+    ei.update(model=model)
+    configurations = [ConfigurationMock([1.0])]
+    with pytest.raises(ValueError):
+        ei(configurations)
 
 def test_ei_1x1(model, acquisition_function):
     ei = acquisition_function
@@ -171,10 +234,6 @@ def test_eic_multi_constraint(model, acq_eic_multi):
 # Test EIPS
 # --------------------------------------------------------------
 
-@pytest.fixture
-def model_eips():
-    return MockModelDual()
-
 
 @pytest.fixture
 def acq_eips(model_eips):
@@ -193,11 +252,44 @@ def test_eips_1xD(model_eips, acq_eips):
     assert np.isclose(acq[0][0], 0.3989422804014327)
 
 
-def test_eips_fail(model_eips, acq_eips):
+def test_eips_m_fail(model, acq_eips):
     eips = acq_eips
+    eips.update(model=model, eta=1.0)
+    configurations = [ConfigurationMock([1.0, 1.0])]
     with pytest.raises(ValueError):
-        configurations = [ConfigurationMock([1.0, 1.0])]
         eips(configurations)
+
+def test_eips_v_fail(model_wrong_v_shape, acq_eips):
+    eips = acq_eips
+    eips.update(model=model_wrong_v_shape, eta=1.0)
+    configurations = [ConfigurationMock([1.0, 1.0]),ConfigurationMock([1.0, 1.0])]
+    with pytest.raises(ValueError):
+        eips(configurations)
+
+
+def test_eips_1d_input(model_eips, acq_eips):
+    eips = acq_eips
+    eips.update(model=model_eips, eta=1.0)
+    configurations = [ConfigurationMock([1.0]), ConfigurationMock([2.0])]
+    acq = eips(configurations)
+
+    assert acq.ndim == 2 
+    assert acq.shape[1] == 1 
+
+def test_eips_eta_fail(model_eips, acq_eips):
+    eips = acq_eips
+    eips.update(model=model_eips)
+    configurations = [ConfigurationMock([1.0, 1.0]), ConfigurationMock([1.0, 1.0])]
+    with pytest.raises(ValueError):
+        eips(configurations)
+
+def test_eips_1xD_zero_var(model_eips_zerovar, acq_eips):
+    eips = acq_eips
+    eips.update(model=model_eips_zerovar, eta=1.0)
+    configurations = [ConfigurationMock([1.0, 1.0]), ConfigurationMock([1.0, 1.0])]
+    acq = eips(configurations)
+    assert acq.shape == (1, 1)
+    assert np.isclose(acq[0][0], 0)
 
 # --------------------------------------------------------------
 # Test LogEI
@@ -209,6 +301,14 @@ def acq_logei(model):
 
 def test_logei_init(acq_logei):
     assert acq_logei.long_name == 'Log Expected Improvement'
+
+
+def test_logei_eta_fail(model, acq_logei):
+    logei = acq_logei
+    logei.update(model=model)
+    configurations = [ConfigurationMock([1.0, 1.0, 1.0])]
+    with pytest.raises(ValueError):
+        logei(configurations)
 
 
 def test_logei_1xD(model, acq_logei):
@@ -233,6 +333,14 @@ def test_logei_NxD(model, acq_logei):
     assert np.isclose(acq[0][0], 1.6670107375002425)
     assert np.isclose(acq[1][0], 1.5570607606556273)
     assert np.isclose(acq[2][0], 0.6480973967332011)
+
+def test_logei_1xD_zero_var(model, acq_logei):
+    logei = acq_logei
+    logei.update(model=model, eta=1.0)
+    configurations = [ConfigurationMock([0.0])]
+    acq = logei(configurations)
+    assert acq.shape == (1, 1)
+    assert np.isclose(acq[0][0], 0)
 
 # --------------------------------------------------------------
 # Test LPEI
@@ -294,6 +402,13 @@ def acq_pi(model):
 def test_pi_init(acq_pi):
     assert acq_pi.long_name == 'Probability of Improvement'
 
+def test_pi_eta_fail(model, acq_pi):
+    pi = acq_pi
+    pi.update(model=model)
+    configurations = [ConfigurationMock([0.5, 0.5, 0.5])]
+    with pytest.raises(ValueError):
+        pi(configurations)
+
 def test_pi_1xD(model, acq_pi):
     pi = acq_pi
     pi.update(model=model, eta=1.0)
@@ -301,15 +416,6 @@ def test_pi_1xD(model, acq_pi):
     acq = pi(configurations)
     assert acq.shape == (1, 1)
     assert np.isclose(acq[0][0], 0.7602499389065233)
-
-
-def test_pi_1xD_zero(model, acq_pi):
-    pi = acq_pi
-    pi.update(model=model, eta=1.0)
-    configurations = [ConfigurationMock([100, 100, 100])]
-    acq = pi(configurations)
-    assert acq.shape == (1, 1)
-    assert np.isclose(acq[0][0], 0)
 
 
 def test_pi_NxD(model, acq_pi):
@@ -339,6 +445,13 @@ def acq_lcb(model):
 def test_lcb_init(acq_lcb):
     assert acq_lcb.long_name == 'Lower Confidence Bound'
     assert acq_lcb.num_data == None
+
+def test_lcb_numdata_fail(model, acq_lcb):
+    lcb = acq_lcb
+    lcb.update(model=model, eta=1.0, par=1)
+    configurations = [ConfigurationMock([0.5, 0.5, 0.5])]
+    with pytest.raises(ValueError):
+        lcb(configurations)
 
 def test_lcb_1xD(model, acq_lcb):
     lcb = acq_lcb
@@ -390,6 +503,14 @@ def acq_uncer(model):
 def test_uncer_init(acq_uncer):
     assert acq_uncer.long_name == 'Uncertainty'
     assert acq_uncer.num_data == None
+
+def test_uncer_numdata_fail(model, acq_uncer):
+    uncertainty = acq_uncer
+    uncertainty.update(model=model)
+    configurations = [ConfigurationMock([0.5])]
+    with pytest.raises(ValueError):
+        uncertainty(configurations)
+
 
 def test_uncertainty_1x1(model, acq_uncer):
     uncertainty = acq_uncer
